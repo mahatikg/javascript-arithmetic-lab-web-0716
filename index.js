fe7521f246243b4edce9a95b3a24449cc6abe277
Mahati Gollamudi <github email address>
2016-08-30 14:26:54 -0400

    JS math complete

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a,b){
+  return a + b;
+}
+
+function subtract(a,b){
+  return a - b;
+}
+
+function multiply(a,b){
+  return a * b;
+}
+
+function divide(a,b){
+  return a / b;
+}
+
+function inc(n){
+  return ++n;
+}
+// console.log(inc(10))
+
+function dec(n){
+  return --n;
+}
+
+function makeInt(n){
+
+  return parseInt(n, 10);
+
+}
+//
+ // console.log(makeInt(30.12))
+
+function preserveDecimal(n){
+
+  return parseFloat(n);
+
+}
+
+ //console.log(preserveDecimal(30.00))
